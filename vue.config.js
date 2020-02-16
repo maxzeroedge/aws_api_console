27070706c0b081e17c45b8d99ae6f78988d03c5e
pc <maxzeroedge@gmail.com>
2020-02-16 20:17:43 +0530

    Updated build scripts

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,49 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  "pluginOptions": {
+    "electronBuilder": {
+      "builderOptions": {
+        "appId": "com.maxzeroedge.aws_api_console",
+        "productName": "AWS API Console",
+        "win": {
+          "target": [
+            {
+              "target": "nsis",
+              "arch": [
+                "x64",
+                "ia32"
+              ]
+            }, 
+            {
+              "target": "portable",
+              "arch": [
+                "x64",
+                "ia32"
+              ]
+            }
+          ]
+        },
+        "linux": {
+          "target": [
+            {
+              "target": "deb",
+              "arch": [
+                "x64",
+                "ia32"
+              ]
+            },
+            {
+              "target": "AppImage",
+              "arch": [
+                "x64",
+                "ia32"
+              ]
+            }
+          ]
+        }
+      }
+    }
+  }
 }
